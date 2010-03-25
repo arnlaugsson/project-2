d537684e09c6b2a6c32c9a8fcf66e6a2e331547b
unknown <Haukur@.(none)>
2010-03-25 22:42:43 +0000

    syncsets part1

--- synchronizingSets.py
+++ synchronizingSets.py
@@ -1,5 +1,9 @@
-program         = ['tc_EOF']
-identifierList  = ['tc_RPAREN','tc_COLON','tc_SEMICOL']
-declarations    = ['tc_FUNCTION','tc_PROCEDURE','tc_BEGIN']
-type            = ['tc_SEMICOL']
-subprogramDeclarations = ['tc_BEGIN','tc_SEMICOL']
+syncsets = {'parseProgram':['tc_EOF'],
+'parseDeclarations':['tc_FUNCTION','tc_PROCEDURE','tc_BEGIN','tc_SEMICOL'],
+'parseIdentifierList':['tc_RPAREN','tc_COLON','tc_SEMICOL'],
+'parseSubprogramDeclarations':['tc_BEGIN','tc_SEMICOL'],
+'parseSubProgramHead':['tc_SEMICOL','tc_VAR','tc_BEGIN'],
+'parseArguments':['tc_COLON','tc_SEMICOL'],
+'parseType':['tc_SEMICOL','tc_VAR','tc_FUNCTION','tc_PROCEDURE'],
+'parseParameterList':['tc_RPAREN','tc_COLON','tc_SEMICOL'],
+'parseCompoundStatement':['tc_DOT','tc_SEMICOL','tc_END','tc_BEGIN']}
